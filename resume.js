221793c92e35a34b807d38428bb68470fa29587f
prabhakararunasalam <142766340+prabhakararunasalam@users.noreply.github.com>
2023-09-06 08:58:30 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,73 @@
+//console.log(`
+    const basicsObj={
+      name: "prabhakar arunachalam",
+      
+      
+      email: "prabhakararunasalam17@gmail.com",
+      phoneno : "7708888802",
+      degree: "bachelor of engineering",
+      website: "none",
+      summary: "A one-sentence to one-paragraph overview text. Do not include any line-breaks.",
+      location: {
+        address: "1/197,indra nagar,mahilvannanathapuram",
+        postalCode: "627808",
+        city: "tenkasi",
+        countryCode: "INDIA",
+        region: "TAMILNADU"
+      },
+    }
+     console.log(basicsObj);
+     const profilesObj = 
+        {
+          network: "linkedin",
+          username: "prabhakar A",
+          url: "https://www.linkedin.com/in/prabhakar-a-962688235"
+        }
+      
+      console.log(profilesObj);
+    
+    
+    const educationObj=
+      {
+        institution: "infant jesus collage of engineering",
+        "area": "Bachelor of enginnering in aeronautical engineering",
+        "studyType": "regulaor",
+        "startDate": "june 2017",
+        "endDate": "march 2021",
+        "gpa": "7.07",
+        "courses": [
+          "ful stack devoloper in guvi geek"
+        ]
+      }
+      console.log(profilesObj);
+    
+    
+    const skillsObj=
+      {
+        name: "programming language",
+        level: "medium",
+        keywords: [
+          "c++,java,javascript"
+        ]
+      }
+    console.log(skillsObj);
+    const languagesObj =
+      {
+        language: "tamil and english",
+        fluency: "tamil- well, english-medium"
+      }
+    console.log(languagesObj);
+
+    const interestsObj =
+      {
+        name: "sports reading book",
+        keywords: [
+          "cricket"
+        ]
+      }
+      console.log(interestsObj);
+
+
+
+    
+    
